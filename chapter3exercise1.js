97ba88750bf946ef8a29561322b6afde1e6095fc
Chad Looker <chad.looker@gmail.com>
2017-09-30 19:10:18 -0400

    Update chapter3exercise1.js

--- chapter3exercise1.js
+++ chapter3exercise1.js
@@ -1,3 +1,10 @@
+/* Write a function min that takes two arguments and returns their minimum.
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10 */
+
 function min(num1, num2) {
   num1 = Number(num1);
   num2 = Number(num2);
