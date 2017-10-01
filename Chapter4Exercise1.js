0f2ce462379a7e416dff9095454833d97b0023e8
Chad Looker <chad.looker@gmail.com>
2017-09-30 20:12:34 -0400

    Update Chapter4Exercise1.js

--- Chapter4Exercise1.js
+++ Chapter4Exercise1.js
@@ -7,8 +7,6 @@ Next, write a sum function that takes an array of numbers and returns the sum of
 
 As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used to build up the array. If no step is given, the array elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 
-// Your code here.
-
 console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(range(5, 2, -1));
