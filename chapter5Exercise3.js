602a78d5e0413abf24a3ab2f43daf6ad5bc0402c
Chad Looker <chad.looker@gmail.com>
2017-10-07 17:14:31 -0400

    Update chapter5Exercise3.js

--- chapter5Exercise3.js
+++ chapter5Exercise3.js
@@ -1,4 +1,4 @@
-*/ Compute and output the average age of the people in the ancestry data set per century. A person is assigned to a century by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100). */
+// Compute and output the average age of the people in the ancestry data set per century. A person is assigned to a century by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100). 
 
 var ancestryArr = [{
     "name": "Carolus Haverbeke",
